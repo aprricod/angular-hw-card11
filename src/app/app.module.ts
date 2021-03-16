1b6580059fd1a915c00e65e05862265996436bb6
Artem <aprricod@gmail.com>
2021-03-16 23:51:35 +0300

    mods comps added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Mod1Module } from './mod1/mod1.module';
+import { Mod2Module } from './mod2/mod2.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Mod1Module,
+    Mod2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
