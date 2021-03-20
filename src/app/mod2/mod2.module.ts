357d99ee078c73c2c13be8be3de67bb77ca7cddd
Artem <aprricod@gmail.com>
2021-03-20 14:01:48 +0300

    material table fixed in mod2

--- src/app/mod2/mod2.module.ts
+++ src/app/mod2/mod2.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { Mod2Component } from './mod2.component';
@@ -10,6 +11,7 @@ import { Mod2Component } from './mod2.component';
   imports: [
     CommonModule,
     MatTableModule,
+    MatInputModule,
     MatFormFieldModule,
     MatPaginatorModule,
   ],
